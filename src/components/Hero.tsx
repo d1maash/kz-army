d880524c59c6633987fb46e13b2cecf580355a16
Dimash <dimashaijarikov@gmail.com>
2025-02-21 22:38:34 +0500

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
                 className="absolute top-0 left-0 w-full h-full z-[-1]"
             />
             <div className="max-w-2xl">
-                <h1 className="text-5xl font-bold text-yellow-400">Служить Родине — долг и честь!</h1>
+                <h1 className="text-5xl font-bold text-yellow-400">Служить Родине — долг и чест</h1>
                 <p className="mt-4 text-lg">Подайте заявку на участие в программах Министерства Обороны</p>
                 <button className="mt-6 border border-yellow-400 text-yellow-400 px-6 py-2 rounded-md text-lg">
                     Подать заявку
