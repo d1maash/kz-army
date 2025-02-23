df29ea3a381f7b1adabda4fe9793e8214cc15db3
zhankeldiulyrasultop1@gmail.com <zhankeldiulyrasultop1@gmail.com>
2025-02-23 16:46:10 +0500

    update

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -5,6 +5,7 @@ import {
     AccordionItem,
     AccordionTrigger,
   } from "@/components/ui/accordion"
+import Link from "next/link"
 
 const FAQPage = () => {
   return (
@@ -16,26 +17,63 @@ const FAQPage = () => {
             <h2 className="text-3xl md:text-4xl font-bold mt-5">Часто задаваемые вопросы</h2>
 
             {/* Accordion */}
-            <Accordion type="single" collapsible className="w-full mt-5 md:px-5">
-                <AccordionItem value="item-1">
-                    <AccordionTrigger className="text-lg font-semibold">Как подать заявку?</AccordionTrigger>
-                    <AccordionContent className="text-base">
-                        Заявку можно подать онлайн через наш сайт или лично в военкомате. Нужно заполнить анкету, прикрепить документы и дождаться обратной связи.
-                    </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-2">
-                    <AccordionTrigger className="text-lg font-semibold">Сколько времени рассматривается заявка?</AccordionTrigger>
-                    <AccordionContent className="text-base">
-                        Обычно проверка документов и первичная проверка занимает до 10 рабочих дней.
-                    </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-3">
-                    <AccordionTrigger className="text-lg font-semibold">Что делать после подачи заявки?</AccordionTrigger>
-                    <AccordionContent className="text-base">
-                        Если ваша заявка одобрена, вас пригласят на собеседование, медицинское обследование и тестирование.
-                    </AccordionContent>
-                </AccordionItem>
-            </Accordion>
+            <div className="flex flex-wrap md:flex-nowrap">
+                <form action="#" className="mt-5">
+                    <input 
+                        type="text" 
+                        placeholder="Поиск"
+                        style={{
+                            backgroundImage: "url('/icons/search.svg')",
+                            backgroundRepeat: "no-repeat",
+                            backgroundPosition: "10px center", 
+                            backgroundSize: "20px 20px", 
+                        }}
+                        className="w-full rounded-xl bg-[#F7F7F7] text-[#858585] p-3 pl-10 "
+                    />
+                    <select
+                        name="education"
+                        className="w-full mt-3 rounded-xl bg-[#F7F7F7] text-black p-3 appearance-none"
+                        style={{
+                            backgroundImage: "url('/icons/down-arrow.svg')", // Add a custom dropdown arrow
+                            backgroundRepeat: "no-repeat",
+                            backgroundPosition: "right 15px center",
+                            backgroundSize: "10px 10px",
+                        }}
+                    >
+                        <option value="">
+                            Направление
+                        </option>
+                        <option value="option1">Связист</option>
+                        <option value="option2">Срочная служба</option>
+                    </select>
+
+                    <button className="mt-6 font-medium w-full rounded-xl bg-custom-yellow py-3">Применить</button>
+                </form>
+
+
+                <Accordion type="single" collapsible className="w-full mt-5 md:px-5">
+                    <AccordionItem value="item-1">
+                        <AccordionTrigger className="text-lg font-semibold">Как подать заявку?</AccordionTrigger>
+                        <AccordionContent className="text-base">
+                            Заявку можно подать онлайн через наш сайт или лично в военкомате. Нужно заполнить анкету, прикрепить документы и дождаться обратной связи.
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-2">
+                        <AccordionTrigger className="text-lg font-semibold">Сколько времени рассматривается заявка?</AccordionTrigger>
+                        <AccordionContent className="text-base">
+                            Обычно проверка документов и первичная проверка занимает до 10 рабочих дней.
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-3">
+                        <AccordionTrigger className="text-lg font-semibold">Что делать после подачи заявки?</AccordionTrigger>
+                        <AccordionContent className="text-base">
+                            Если ваша заявка одобрена, вас пригласят на собеседование, медицинское обследование и тестирование.
+                        </AccordionContent>
+                    </AccordionItem>
+                </Accordion>
+            </div>
+
+                <Link href="/faq/ask-question" className="fixed right-20 bottom-10 animate-bounce text-center font-medium w-full max-w-[200px] rounded-xl bg-custom-yellow py-3">Задать вопрос</Link>
         </div>
     </>
   )
---
+++ src/components/FaqProfileCard.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image"
+
+interface FaqProfileCardType {
+  name: string;
+  role: string;
+  image: string
+}
+
+const FaqProfileCard = ({ name, role, image }: FaqProfileCardType) => {
+  return (
+    <div className="w-full py-3 px-5 flex flex-col items-center justify-center bg-[#F7F7F7] rounded-xl">
+        <Image 
+          src={image}
+          alt="profile"
+          width={200}
+          height={200}
+        />
+        <h3 className="mt-3 text-center font-bold text-lg">{name}</h3>
+        <p className="text-center font-medium">{role}</p>
+        <button className="w-full font-medium mt-5 py-3 rounded-xl border-2 border-custom-yellow ">Задать вопрос</button>
+    </div>
+  )
+}
+
+export default FaqProfileCard
